fe838885e57f55ce19b8adb6a172e2e06520f25c
Afrijal Dzuhri <ijuldz@gmail.com>
2023-06-06 19:59:54 +0700

    fix: redirect url on submit

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,20 @@ import { ref } from "vue";
 const number = ref("");
 
 function go() {
-  console.log(number.value);
+  if (number.value[0] === "0") {
+    number.value = number.value.slice(1);
+  }
+
+  window.location.replace(`https://wa.me/62${number.value}`);
 }
 
 function handleChange(e: KeyboardEvent) {
   if (
     (e.keyCode >= 48 && e.keyCode <= 57) ||
-    (e.keyCode >= 96 && e.keyCode <= 105)
+    (e.keyCode >= 96 && e.keyCode <= 105) ||
+    e.keyCode === 8
   ) {
     // 0-9 only
-    console.log((e.target as HTMLInputElement).value);
     number.value = (e.target as HTMLInputElement).value;
   } else {
     e.preventDefault();
@@ -34,6 +38,9 @@ function handleChange(e: KeyboardEvent) {
         @keydown="handleChange"
         v-model="number"
       />
+      <span class="text-xs text-gray-500"
+        >Without zero number in the beginning</span
+      >
       <div class="mt-4">
         <button
           class="bg-green-500 py-3 px-5 text-white w-full rounded"
