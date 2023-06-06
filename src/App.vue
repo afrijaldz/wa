99caff43e0a7df0e156319e19279239c9fa2b5bd
Afrijal Dzuhri <ijuldz@gmail.com>
2023-06-06 19:14:12 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const number = ref("");
+
+function go() {
+  console.log(number.value);
+}
+
+function handleChange(e: Event) {
+  number.value = (e.target as HTMLInputElement).value;
+}
+</script>
+
+<template>
+  <div
+    class="h-screen w-screen bg-green-100 flex items-center justify-center flex-col"
+  >
+    <div class="my-5">This tool helps you to go the whatsapp chat easily.</div>
+    <div class="w-[500px] bg-white p-5 rounded">
+      <img class="my-5 mx-auto" src="../src/assets/whatsapp.svg" />
+      <input
+        class="py-5 px-3 border w-full focus-visible:outline-green-900"
+        placeholder="Input whatsapp number"
+        @change.prevent="handleChange"
+      />
+      <div class="mt-4">
+        <button
+          class="bg-green-500 py-3 px-5 text-white w-full rounded"
+          @click="go"
+        >
+          Go
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
